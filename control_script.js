023f82a47b4275b4babee41b77c28c8384f223c1
techn0gmer <techn0gmer@gmail.com>
2021-06-18 23:20:22 -0500

    created maid webpage with basic formating and started some terrible js code for user configuration

---
+++ control_script.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+document.getElementById("width_box").value = 500;
+document.getElementById("height_box").value = 500;
+
+function resizeCanvas() {
+
+    var width = document.getElementById("width_box").value;
+    var height = document.getElementById("height_box").value;
+
+    console.log("User Input: " + width + "x" + height);
+
+    canvas.width = width;
+    canvas.height = height;
+
+    console.log("Canvas resized to " + width + "x" + height);
+
+}
